256255fc3fbdc85e6b0aa072796ff095b7aac2e4
Julian Trupe <juliantrupe@gmail.com>
2019-10-26 11:57:15 -0700

    merge/changes to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,39 @@
 $(document).ready(function () {
 
-    //input form variables
-    var nameInput = $(".name-input").val().trim();
-    var roleInput = $(".role-input").val().trim();
-    var dateInput = $(".start-date-input").val().trim();
-    var rateInput = $(".rate-input").val().trim();
+    var config = {
+        apiKey: "AIzaSyA7W5R5wt-lIy9HZokVKM3rRNV6SdjPetM",
+        authDomain: "timesheet-manager-b9e4a.firebaseapp.com",
+        databaseURL: "https://timesheet-manager-b9e4a.firebaseio.com",
+        projectId: "timesheet-manager-b9e4a",
+        storageBucket: "timesheet-manager-b9e4a.appspot.com",
+        messagingSenderId: "195420129592",
+        appId: "1:195420129592:web:f921eba976f0b8665b59cd"
+    }
 
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    var employee = {
+        name: "",
+        role: "",
+        startDate: "",
+        rate: "",
+    }
+
+    //input form variables
+    employee.name = $(".name-input").val().trim();
+    employee.role = $(".role-input").val().trim();
+    employee.startDate = $(".start-date-input").val().trim();
+    employee.rate = $(".rate-input").val().trim();
+
+
+    database.ref().push({
+        name: employee.name,
+        role: employee.role,
+        startDate: employee.startDate,
+        rate: employee.rate,
+    })
     //table header variables
 
     var nameDisplay = $(".name")
